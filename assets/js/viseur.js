607111257eda66da9356a2b0e19e0548e4c645a5
johan-laget <johan.laget@laplateforme.io>
2024-04-21 17:02:05 +0200

    push des 2 js viseur et main

--- assets/js/viseur.js
+++ assets/js/viseur.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
     var isInterfaceVisible = localStorage.getItem("interfaceVisible");
     if (isInterfaceVisible === "true") {
         document.getElementById("interfaceparametre").style.display = "block";
-  }
-  document.getElementById("boutonparametre").onclick = function() {
+    }
+    document.getElementById("boutonparametre").onclick = function() {
         var interfaceParametre = document.getElementById("interfaceparametre");
         if (interfaceParametre.style.display === "none" || !interfaceParametre.style.display) {
             interfaceParametre.style.display = "block";
@@ -13,72 +13,46 @@ document.addEventListener("DOMContentLoaded", function() {
             localStorage.setItem("interfaceVisible", "false");
         }
     };
-  });
-  
-  
-  const cursorImages = [
-    './assets/css/viseur0.png',
-    './assets/css/viseur1.png',
-    './assets/css/viseur2.png',
-    './assets/css/viseur3.png',
-    './assets/css/viseur4.png',
-    './assets/css/viseur5.png'
-  ];
-  const changeCursorBtn = document.getElementById('changeCursorBtn');
-  let currentCursorIndex = 0;
-  function changeCursor() {
-    // Changer le curseur en fonction de l'index actuel
-    document.body.style.cursor = `url('${cursorImages[currentCursorIndex]}'), auto`;
-    // Incrémenter l'index pour passer à l'image de curseur suivante
-    currentCursorIndex++;
-    // Si on a atteint la fin de la liste, revenir au début
-    if (currentCursorIndex === cursorImages.length) {
-        currentCursorIndex = 0;
-    }
-  }
-  
-  // Écouteur d'événement pour le clic sur le bouton
-  changeCursorBtn.addEventListener('click', changeCursor);
-  
-  // Appeler la fonction changeCursor au chargement de la page pour définir un curseur par défaut
-  changeCursor();
-  
-  // ****
-  // Récupération des éléments sur lesquels vous voulez conserver le curseur actuel
-  const buttonElements = document.querySelectorAll('.bouton_index a');
-  const navElements = document.querySelectorAll('.nav_element');
-  
-  // Gestionnaire d'événement pour conserver le curseur actuel lorsque vous survolez les boutons
-  buttonElements.forEach(button => {
-      button.addEventListener('mouseover', () => {
-          document.body.style.cursor = 'default';
-      });
-      button.addEventListener('mouseout', () => {
-          document.body.style.cursor = 'auto';
-      });
-  });
-  
-  // Gestionnaire d'événement pour conserver le curseur actuel lorsque vous cliquez sur les boutons
-  buttonElements.forEach(button => {
-      button.addEventListener('click', () => {
-          document.body.style.cursor = 'default';
-      });
-  });
-  
-  // Gestionnaire d'événement pour conserver le curseur actuel lorsque vous survolez les éléments de navigation
-  navElements.forEach(nav => {
-      nav.addEventListener('mouseover', () => {
-          document.body.style.cursor = 'default';
-      });
-      nav.addEventListener('mouseout', () => {
-          document.body.style.cursor = 'auto';
-      });
-  });
-  
-  // Gestionnaire d'événement pour conserver le curseur actuel lorsque vous cliquez sur les éléments de navigation
-  navElements.forEach(nav => {
-      nav.addEventListener('click', () => {
-          document.body.style.cursor = 'default';
-      });
-  });
-  
+});
+
+// window.addEventListener('DOMContentLoaded', (event) => {
+//     const body = document.querySelector('body');
+//     body.style.cursor = `url('./assets/css/viseur0.png'), auto`;
+// });
+// const changeCursorBtn = document.getElementById('changeCursorBtn');
+// const body = document.querySelector('body');
+
+// const cursorImages = [
+//     './assets/css/viseur0.png',
+//     './assets/css/viseur1.png',
+//     './assets/css/viseur2.png',
+//     './assets/css/viseur3.png',
+//     './assets/css/viseur4.png',
+//     './assets/css/viseur5.png'
+// ];
+
+// let currentCursorIndex = localStorage.getItem('cursorIndex') || 0;
+
+// function changeCursor() {
+//     if (currentCursorIndex === cursorImages.length - 1) {
+//         currentCursorIndex = 0;
+//     } else {
+//         currentCursorIndex++;
+//     }
+//     const cursorImage = cursorImages[currentCursorIndex];
+//     document.body.style.cursor = `url('${cursorImage}')15 15, auto`;
+//     localStorage.setItem('cursorIndex', currentCursorIndex);
+// }
+// const changeCursorBtnIndex = document.getElementById('changeCursorBtn');
+// if (changeCursorBtnIndex) {
+//     changeCursorBtnIndex.addEventListener('click', changeCursor);
+// }
+// const changeCursorBtnGame = document.getElementById('changeCursorBtnGame');
+// if (changeCursorBtnGame) {
+//     changeCursorBtnGame.addEventListener('click', changeCursor);
+// }
+// window.addEventListener('DOMContentLoaded', () => {
+//     const savedCursorIndex = localStorage.getItem('cursorIndex') || 0;
+//     const cursorImage = cursorImages[savedCursorIndex];
+//     document.body.style.cursor = `url('${cursorImage}'), auto`;
+// });
